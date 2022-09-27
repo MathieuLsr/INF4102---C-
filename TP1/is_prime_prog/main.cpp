cbb77676cab7da0f6edd379a0bd18eaaaea5c725
MathieuLsr <94841682+MathieuLsr@users.noreply.github.com>
2022-09-27 10:06:41 +0200

    Add files via upload

---
+++ TP1/is_prime_prog/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>  
+#include <tgmath.h>
+
+using namespace std;  
+int main()  
+{  
+    int n, i, m=0, flag=0;  
+    cout << "Enter the Number to check Prime: ";  
+    cin >> n;  
+    //  m=n/2;  
+    for(i = 2; i <= sqrt(n); i++) {  
+        if(n % i == 0) {  
+            flag=1;  
+            break;  
+        }
+    } 
+    cout <<  n << " is a prime: " << boolalpha << (flag == 0) <<endl ;  
+    return 0;  
+}  
