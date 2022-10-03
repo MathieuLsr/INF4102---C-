9dae3b6a35076e4f12820b01ad4a1128b0ff2bbc
MathieuLsr <94841682+MathieuLsr@users.noreply.github.com>
2022-10-03 15:50:54 +0200

    Update main.cpp

--- is_prime_prog/main.cpp
+++ is_prime_prog/main.cpp
@@ -4,17 +4,17 @@
 using namespace std;  
 int main()  
 {  
+    int i ;  
     long long n ;
-    int i, m=0, flag=0;  
     cout << "Enter the Number to check Prime: ";  
     cin >> n;  
-    //  m=n/2;  
-    for(i = 2; i <= sqrt(n); i++) {  
+    for(i = 2; i <= sqrt(n); i++)  
         if(n % i == 0) {     
-            flag=1;  
-            break;  
+            cout << n << " is a prime: False" << endl ;
+            return 0 ; 
         }
-    } 
-    cout <<  n << " is a prime: " << boolalpha << (flag == 0) <<endl ;  
+
+    cout << n << " is a prime: True" << endl ;
+    //cout <<  n << " is a prime: " << boolalpha << (flag == 0) <<endl ;  
     return 0;  
 }  
